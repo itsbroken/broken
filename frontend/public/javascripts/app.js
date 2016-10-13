e9ea596d8b6932f45e6edc8a7e1d46450942f2b0
John Yong <whipermr5@gmail.com>
2016-10-13 16:37:18 +0800

    Used location.host to build websocket address instead of hardcoding

--- frontend/public/javascripts/app.js
+++ frontend/public/javascripts/app.js
@@ -1,4 +1,4 @@
-var ws = new WebSocket("ws://localhost:8888/websocket");
+var ws = new WebSocket("ws://" + location.host + "/websocket");
 
 ws.onmessage = function (event) {
   data = JSON.parse(event.data);
