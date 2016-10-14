26b6eda3ec6bf4477bc9916dac0eedcb851286de
John Yong <whipermr5@gmail.com>
2016-10-15 01:58:13 +0800

    Fixed wrong sys path

--- frontend/app.py
+++ frontend/app.py
@@ -6,7 +6,7 @@ import json
 import os.path
 
 import sys
-sys.path.append('../broken')
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from broken import utils, store
 
 zmq.eventloop.ioloop.install()
